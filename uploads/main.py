28ff03cab144e00fed9b3a2480365bee93790606
jachos254 <jachowski.m@gmail.com>
2022-10-18 13:22:18 +0200

    updated bootstrap url

--- uploads/main.py
+++ uploads/main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/templates', methods = ['GET', 'POST'])
 def upload_image():
     return render_template('index.html')
 

ab88ad4a254a284a97ce5365473d7a4312d12439
jachos254 <jachowski.m@gmail.com>
2022-10-20 14:40:40 +0200

    fighting with image upload

--- flask01/main.py
+++ flask01/main.py
@@ -6,7 +6,7 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1337'
+app.config['SECRET_KEY'] = 'sdfgdshdf'
 app.config['UPLOADED_PHOTOS_DEST'] = 'uploads'
 
 photos = UploadSet('photos', IMAGES)
@@ -21,12 +21,13 @@ class UploadForm(FlaskForm):
     )
     submit = SubmitField('Upload')
 
+
 @app.route('/uploads/<filename>')
 def get_file(filename):
     return send_from_directory(app.config['UPLOADED_PHOTOS_DEST'], filename)
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def upload_image():
     form = UploadForm()
     if form.validate_on_submit():
@@ -37,4 +38,4 @@ def upload_image():
     return render_template('index.html', form=form, file_url=file_url)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080, use_reloader=False)
